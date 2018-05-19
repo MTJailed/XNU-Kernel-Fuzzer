ce11eafce8f3496fbe786bd5c80c29952ca6a384
Sem <jailed@sems-mac.local>
2018-05-19 15:37:55 -0700

    Initial Commit

---
+++ XNUFuzzer/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  XNUFuzzer
+//
+//  Created by Sem Voigtländer on 5/19/18.
+//  Copyright © 2018 Sem Voigtländer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
